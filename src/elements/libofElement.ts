4fe972e97bc46379c6dadb7af793d063f8655e6b
Gonzalo Rando Serna <gonzalorando03@gmail.com>
2024-03-24 16:07:35 +0100

    v1.1.2

--- src/elements/libofElement.ts
+++ src/elements/libofElement.ts
@@ -3,9 +3,9 @@ export class LibofElement {
     color:string
     backgroundColor:string
     font:string
-    fontSize:number
+    fontSize:number | null
     
-    constructor(content:string, color = 'black', backgroundColor = 'transparent',fontSize = 12, font = 'Arial'){
+    constructor(content:string, color = 'black', backgroundColor = 'transparent',fontSize:number | null = null, font = 'Arial'){
         this.content = content
         this.color = color
         this.backgroundColor = backgroundColor
@@ -14,10 +14,15 @@ export class LibofElement {
     }
 
     getStyles(){
-        return 'style="color:' + this.color + 
+        const stylesBase = 'style="color:' + this.color + 
         '; background-color:' + this.backgroundColor +
-        '; font-family:' + this.font +'; font-size:' + 
-        this.fontSize + 'px;"'
+        '; font-family:' + this.font +';'
+
+        const endStyles = this.fontSize === null
+            ? '"'
+            : ' font-size:' + this.fontSize + 'pt;"'
+
+        return stylesBase + endStyles
     }
 
     getValue(){
